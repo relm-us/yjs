4a657fa1df903a10f145bcb4a3fc85195abb169d
Duane Johnson <duane.johnson@gmail.com>
2020-07-12 09:26:08 -0600

    Add YDoc toJSON

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,12 @@
 
-import * as array from './y-array.tests.js'
+import * as doc from './y-doc.tests.js'
 import * as map from './y-map.tests.js'
+import * as array from './y-array.tests.js'
 import * as text from './y-text.tests.js'
 import * as xml from './y-xml.tests.js'
+import * as consistency from './consistency.tests.js'
 import * as encoding from './encoding.tests.js'
 import * as undoredo from './undo-redo.tests.js'
-import * as consistency from './consistency.tests.js'
 
 import { runTests } from 'lib0/testing.js'
 import { isBrowser, isNode } from 'lib0/environment.js'
@@ -15,7 +16,7 @@ if (isBrowser) {
   log.createVConsole(document.body)
 }
 runTests({
-  map, array, text, xml, consistency, encoding, undoredo
+  doc, map, array, text, xml, consistency, encoding, undoredo
 }).then(success => {
   /* istanbul ignore next */
   if (isNode) {
